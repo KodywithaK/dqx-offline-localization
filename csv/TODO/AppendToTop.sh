e923e68522d190ccc08d9f94a024741c84dc91fe
KodywithaK <69839708+KodywithaK@users.noreply.github.com>
2023-08-11 20:40:24 -0700

    csv

--- csv/TODO/AppendToTop.sh
+++ csv/TODO/AppendToTop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# Put in pakchunk0-Switch_P/**/StringTables/GAME/
 
 DIR=`dirname ${BASH_SOURCE[0]}`
 for f in $DIR/*.txt $DIR/**/*.txt $DIR/**/**/*.txt $DIR/**/**/**/*.txt $DIR/**/**/**/**/*.txt; do
 	echo -e "Processing $f...\n";
-	echo -e "Key, SourceString\n$(cat $f)" > $f;
+	echo -e "Key, SourceString\n$(cat $f)" > $f.csv;
 done
